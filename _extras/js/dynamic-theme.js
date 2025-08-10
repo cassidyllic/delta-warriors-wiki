d9f702c2f5799aac0bc90f33b8c09d256764360f
Cassidy <cassidy-vassal@outlook.com>
2025-08-11 00:14:21 +0200

    Deployed ba3791b with MkDocs version: 1.6.1

--- _extras/js/dynamic-theme.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const path =
-    window.location.pathname
-      .replace(/\/$/, "")
-      .replace(/^\/|\/$/g, "")
-      .replace(/\//g, "-") || "index";
-
-  document.body.classList.add(`page-${path}`);
-});
